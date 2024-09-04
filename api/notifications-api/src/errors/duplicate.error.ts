74966e7636d8af833feb187f011e89df43e9b1a8
Pedro Wood <pedro.wood@truenorth.co>
2024-09-04 15:38:00 -0300

    remove unused error

--- api/notifications-api/src/errors/duplicate.error.ts
+++
@@ -1,8 +0,0 @@
-import { BusinessError } from './business.error';
-import { ErrorType } from './error-type.enum';
-
-export class DuplicateError extends BusinessError {
-  constructor(message: string) {
-    super(ErrorType.Duplicate, message);
-  }
-}
